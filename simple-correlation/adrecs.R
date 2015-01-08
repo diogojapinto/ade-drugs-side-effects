cfb157ef396fe5633962d9c1cc2c0be79ce7be14
Pedro Costa <pedro.vendascosta@gmail.com>
2015-01-08 22:42:31 +0000

    Joining drug with synonyms so they can be compared with the ndc database

--- simple-correlation/adrecs.R
+++ simple-correlation/adrecs.R
@@ -2,7 +2,7 @@ library(RMySQL)
 
 adrecsConn <- dbConnect(MySQL(), user='pcosta', password='pcosta', dbname='ADReCS', host='porto.fe.up.pt')
 
-rs = dbSendQuery(adrecsConn, "SELECT * FROM drug")
-drugs = fetch(rs, n=-1)
+rs <- dbSendQuery(adrecsConn, "SELECT drug.id, drug.name, drug_synonym.syn FROM drug JOIN drug_synonym ON drug.id = drug_synonym.drug")
+drugs <- fetch(rs, n=-1)
 
-drugs
+head(drugs)
