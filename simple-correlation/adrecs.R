6df2c7f04fce454e3f323219ede1243fc09988d8
Pedro Costa <pedro.vendascosta@gmail.com>
2015-01-10 11:28:16 +0000

    Created some useful functions to query the database

--- simple-correlation/adrecs.R
+++ simple-correlation/adrecs.R
@@ -1,8 +1,32 @@
 library(RMySQL)
 
-adrecsConn <- dbConnect(MySQL(), user='pcosta', password='pcosta', dbname='ADReCS', host='porto.fe.up.pt')
+query <- function(con, query, n=-1){
+	res <- fetch(dbSendQuery(con,query),n=n)
+	return(res)
+}
 
-rs <- dbSendQuery(adrecsConn, "SELECT drug.id, drug.name, drug_synonym.syn FROM drug JOIN drug_synonym ON drug.id = drug_synonym.drug")
+getDrugSynonyms <- function(con, drugId){
+	query <- paste('SELECT * FROM drug_synonym WHERE drug like "', drugId, sep="")
+	query <- paste(query, '"', sep="")
+	syns <- query(con, query)
+	return(syns)
+}
+
+getDrugs <- function(con){
+	query <- "SELECT * FROM drug"
+	drugs <- query(con,query)
+	return(drugs)
+}
+
+getConnection <- function(user='pcosta', password='pcosta', dbname='ADReCS', host='porto.fe.up.pt'){
+	adrecsConn <- dbConnect(MySQL(), user=user, password=password, dbname=dbname, host=host)
+	return(adrecsConn)
+}
+
+
+rs <- dbSendQuery(getConnection(), "SELECT drug.id, drug.name, drug_synonym.syn FROM drug JOIN drug_synonym ON drug.id = drug_synonym.drug")
 drugs <- fetch(rs, n=-1)
 
 head(drugs)
+head(getDrugSynonyms(getConnection(), "BADD_D00002"))
+head(getDrugs(getConnection()))
