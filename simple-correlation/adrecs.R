759ebc0103b552ce294a8d90a0f2c8073fbdb09b
Pedro Costa <pedro.vendascosta@gmail.com>
2015-01-10 11:42:57 +0000

    Getting drugs given a name. Joining drugs and synonyms tables

--- simple-correlation/adrecs.R
+++ simple-correlation/adrecs.R
@@ -23,10 +23,20 @@ getConnection <- function(user='pcosta', password='pcosta', dbname='ADReCS', hos
 	return(adrecsConn)
 }
 
+getDrugsByName <- function(con, drugName){
+	qs <- c('SELECT DISTINCT(drug.id) FROM drug JOIN drug_synonym ON drug.id = drug_synonym.drug WHERE drug.name like "', drugName, '" OR drug_synonym.syn like "', drugName, '"')
+	query <- paste(qs, collapse="") 
+	res <- query(con, query)
+	return(res)
+}
+
 
 rs <- dbSendQuery(getConnection(), "SELECT drug.id, drug.name, drug_synonym.syn FROM drug JOIN drug_synonym ON drug.id = drug_synonym.drug")
 drugs <- fetch(rs, n=-1)
 
+con <- getConnection()
+
 head(drugs)
-head(getDrugSynonyms(getConnection(), "BADD_D00002"))
-head(getDrugs(getConnection()))
+head(getDrugSynonyms(con, "BADD_D00002"))
+head(getDrugs(con))
+head(getDrugsByName(con, "Plenaxis"))
