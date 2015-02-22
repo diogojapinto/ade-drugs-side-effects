13c659ffcb8e5d7ce349616846718fd23af2bb2a
Pedro Costa <pedro.vendascosta@gmail.com>
2015-02-22 15:18:24 +0000

    Preparing for a more informed decision about the importance of pmids by getting the sum of occurences of each pmid

--- simple-correlation/cleaner.R
+++ simple-correlation/cleaner.R
@@ -11,6 +11,21 @@ getPopularPMIDs <- function(threshold) {
   if (file.exists("helpers/popular_pmids.R")) {
     load("helpers/popular_pmids.R")
   } else {
+    pmids <- loadPMIDsCountByDrug()
+  }
+  
+  selection <- pmids$count >= threshold
+  return(pmids[selection,])
+}
+
+##
+## Retrieves the number of occurences of pmids across all drugs that are greater than a threshold
+##
+getTotalPMIDsCount <- function(threshold){
+
+  if (file.exists("helpers/total_pmids_count.R")) {
+    load("helpers/total_pmids_count.R")
+  } else {
     pmids <- loadPMIDsCount()
   }
   
@@ -19,9 +34,9 @@ getPopularPMIDs <- function(threshold) {
 }
 
 ##
-## For each pmid, retrieves the number of documents it appears in
+## For each pmid, retrieves the number of drugs it appears in
 ##
-loadPMIDsCount <- function() {
+loadPMIDsCountByDrug <- function() {
   
   # make cluster
   cl <- makeCluster(detectCores() - 1)
@@ -48,6 +63,41 @@ loadPMIDsCount <- function() {
   pmids <- aggregate(count ~ pmids, FUN="sum", data=pmids)
   
   filename <- "helpers/popular_pmids.R"
+  save(pmids, file=filename)
+  
+  return(pmids)
+}
+
+
+##
+## For each pmid, retrieves the number of occurences
+##
+loadPMIDsCount <- function() {
+  
+  # make cluster
+  cl <- makeCluster(detectCores() - 1)
+  registerDoParallel(cl, cores = detectCores() - 1)
+  
+  availableRecords <- list.files("pmids/")
+  nr_drugs <- length(availableRecords)
+  pmids <- data.frame(pmids=character(0), count=numeric(0))
+  
+  # do parallel computation
+  pmids = foreach(record = availableRecords, .packages=c(), .combine=rbind) %dopar% {
+    try({
+      print(paste("Processing", record))
+      load(paste(c("pmids/", record), collapse=""))
+      
+      data.frame(pmids=pmids$pmid, count=rep_len(1, length(pmids$pmid)))
+    })
+  }
+  
+  stopCluster(cl)
+  
+  print("aggregating...")
+  pmids <- aggregate(count ~ pmids, FUN="sum", data=pmids)
+  
+  filename <- "helpers/total_pmids_count.R"
   save(pmids, file=filename)
   
   return(pmids)
