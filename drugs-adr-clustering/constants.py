3b48dec6864a223a4a08509549adf66d5190e78b
Diogo Pinto <diogojapinto@gmail.com>
2015-03-15 14:42:07 +0000

    done testing function, now debug part

--- drugs-adr-clustering/constants.py
+++ drugs-adr-clustering/constants.py
@@ -18,3 +18,8 @@ ENERGY_TO_RETAIN = 0.9
 LEARNING_RATE = 0.3
 REGULARIZATION_PARAM = 0.5    # trading of between predicted performance on the model
                                   # (high value), versus the complexity of the model (low value)
+
+## Testing related:
+NR_ITERATIONS = 1000
+MIN_TO_KEEP = 0.3
+MAX_TO_KEEP = 0.7
--- drugs-adr-clustering/prediction_main.py
+++ drugs-adr-clustering/prediction_main.py
@@ -11,6 +11,9 @@ from utils import get_training_and_test_sets
 from sys import argv
 import latent_factors_utils as lf
 import pandas
+from constants import MAX_TO_KEEP, MIN_TO_KEEP, NR_ITERATIONS
+import numpy as np
+import scipy as sp
 
 def main():
     """ Entry function.
@@ -52,7 +55,7 @@ def main():
     p_mat, q_mat = lf.gradient_descent(matrix, p_mat, q_mat, testing)
 
     # test things out
-    test_latent_factors(q_mat, )
+    test_latent_factors(q_mat, test_set)
 
     # Return the matrixes with the corresponding indexes
     u_df = pandas.DataFrame(p_mat, index=drugs)
@@ -60,6 +63,53 @@ def main():
 
     return u_df, v_df
 
+def test_latent_factors(q_mat, test_set):
+    """ Computes the average error of the obtained latent factors model
+        based on the average root_mean_square error of the test_set """
+    errors = []
+    test_set = test_set.as_matrix()
+
+    for _ in range(NR_ITERATIONS):
+        line_i = np.random.randint(0, len(test_set))
+        original_obj = test_set[line_i]
+        obj = original_obj.copy
+
+
+        # put some of them in 0
+        zeroed_elems_ratio = (MAX_TO_KEEP - MIN_TO_KEEP) * np.random.random_sample() + MIN_TO_KEEP
+        candidates = obj > 0
+
+        for index, elem in enumerate(candidates):
+            if elem == False:
+                continue
+            prob = np.random.random_sample()
+            if prob <= zeroed_elems_ratio:
+                obj[index] = 0
+
+        obj_factors = obj.dot(q_mat.transpose())
+        new_obj = obj_factors.dot(q_mat)
+
+        errors.append(lf.compute_rmse(original_obj, new_obj))
+
+    print("Mean : {0:8.6f}".format(sp.mean(errors)))
+    print("Minimum : {0:8.6f}".format(min(errors)))
+    print("Maximum : {0:8.6f}".format(max(errors)))
+    print("Variance : {0:8.6f}".format(sp.var(errors)))
+    print("Std. deviation : {0:8.6f}".format(sp.std(errors)))
+
 
 if __name__ == "__main__":
     main()
+
+
+"""
+funcoes a implementar:
+    calcular distância entre drugs (cosine distance entre drug * adr2concept)
+    calcular distância entre adrs (cosine distance entre adr * drug2concept)
+
+    cluster de adrs (baseado no cosine distance dos concepts(k-means, k=nº de adrs pode resolver isto))
+    cluster de drugs (baseado no cosine distance dos concepts(k-means, k=nº de drugs pode resolver isto))
+
+    prever adrs de drugs (drug * adr2concept * adr2concept^T)
+    prever drugs que têm uma dada adr (adr * drug2concept * drug2concept^T)
+"""
