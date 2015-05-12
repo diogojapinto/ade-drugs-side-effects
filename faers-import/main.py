70e33f8f3f1df0ba25b9f433082cb5e10e4f4466
diogojapinto <diogojapinto@gmail.com>
2015-05-12 16:04:01 +0100

    loading of new faers model done

--- faers-import/main.py
+++ faers-import/main.py
@@ -1,10 +1,13 @@
 """
     Loads the FAERS data onto a RDBS
+
+    refer to the files ASC_NTS on each trimester folder
 """
 
 import mysql.connector as mysql
 import os
 import re
+import pandas as pd
 
 def main():
     """ Main function here """
@@ -21,27 +24,68 @@ def main():
                     drugs_parser,
                     reactions_parser,
                     outcomes_parser,
-                    reports_sources_parser,
+                    report_sources_parser,
                     therapy_parser,
                     indications_parser]
 
     data_root = './data'
 
     for quarter_folder in os.listdir(data_root): 
-        is_new = False
+        is_new_model = False
         match = re.match(r'^faers\w*', quarter_folder.lower())
-        if match:
-            is_new = True
 
-        files_dir = data_root + '/' + quarter_folder
+        if match:
+            is_new_model = True
+
+        files_dir = data_root + '/' + quarter_folder 
 
         for in_file in os.listdir(files_dir):
-            
+            for pattern, parser in zip(file_patterns, file_parsers):
+                match = re.match(pattern, in_file)
+                if match:
+                    file_path = files_dir + '/' + in_file
+                    parser(file_path, is_new_model)
 
-        for pat, fn in zip(file_patterns, file_parsers):
-            match = re.match(pat, in_file)
-            if (match)
+def demographic_parser(filepath, is_new_model):
+    """ Parses and inserts the data from the demographic FAERS files """
+    if is_new_model:
+        generic_parser(filepath)
 
+def drugs_parser(filepath, is_new):
+    """ Parses and inserts the data from the drugs FAERS files """
+    if is_new_model:
+        generic_parser(filepath)
+
+def reactions_parser(filepath, is_new):
+    """ Parses and inserts the data from the reactions FAERS files """
+    if is_new_model:
+        generic_parser(filepath)
+
+def outcomes_parser(filepath, is_new):
+    """ Parses and inserts the data from the outcomes FAERS files """
+    if is_new_model:
+        generic_parser(filepath)
+
+def report_sources_parser(filepath, is_new):
+    """ Parses and inserts the data from the report sources FAERS files """
+    if is_new_model:
+        generic_parser(filepath)
+
+def therapy_parser(filepath, is_new):
+    """ Parses and inserts the data from the therapy FAERS files """
+    if is_new_model:
+        generic_parser(filepath)
+    
+def indications_parser(filepath, is_new):
+    """ Parses and inserts the data from the indications FAERS files """
+    if is_new_model:
+        generic_parser(filepath)
+
+def generic_parser(filepath):
+    data_frame = pd.read_csv(filepath, header=0, sep='$')
+    cnx = mysql.connect(user='dpinto', password='dpinto', host='porto.fe.up.pt', database='medline')
+    data_frame.to_sql(con=cnx, name='event', if_exists='replace', flavor='mysql')
+    cnx.close()
 
 if __name__ == '__main__':
     main()
